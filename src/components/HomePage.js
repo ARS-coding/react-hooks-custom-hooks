2a4ca8f2d01f24335ee4c208fbaa78f4988051c0
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-09 11:47:19 +0300

    Connect the custom hook named useQuery to the HomePage component.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,29 +2,17 @@ import React, { useEffect, useState } from "react";
 import About from "./About";
 import ArticleList from "./ArticleList";
 import useDocumentTitle from "../hooks/useDocumentTitle";
+import useQuery from "../hooks/useQuery";
 
 function HomePage() {
-  // fetch data for posts
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch("http://localhost:3000/posts")
-      .then((r) => r.json())
-      .then((posts) => {
-        setPosts(posts);
-        setIsLoaded(true);
-      });
-  }, []);
-
+  const { data, isLoaded } = useQuery("http://localhost:3000/posts");
   // set the document title
   useDocumentTitle()
 
   return (
     <>
       <About />
-      {isLoaded ? <ArticleList posts={posts} /> : <h3>Loading...</h3>}
+      {isLoaded ? <ArticleList posts={data} /> : <h3>Loading...</h3>}
     </>
   );
 }
